c83c4a3e2fcc2428677c40379f65f54af57062d9
hamza <hamzazizkhan@gmail.com>
2023-12-28 22:13:29 +0100

    anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+const rand = ({max, min}) => {
+  return(
+    Math.floor(Math.random()*(max-min+1)) +min
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const len = anecdotes.length
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(Array(len).fill(0))
+
+  const anec = anecdotes[selected]
+  const ind = anecdotes.indexOf(anec)
+  const currentVote = vote[ind]
+
+  const mostVotesAmount = Math.max(...vote)
+  const mostVotesInd = vote.indexOf(mostVotesAmount)
+  const mostVotesAnec = anecdotes[mostVotesInd]
+  
+  const nums = {
+    max:7,
+    min:0
+  }
+
+  const handleVoteclick= () => {
+    const updatedVote = [...vote]
+    updatedVote[anecdotes.indexOf(anec)] += 1
+    console.log(updatedVote)
+    setVote(updatedVote)
+  }
+  const handleAnecdoteClick = () => {
+    setSelected(rand(nums))
+  }
+  
+  
+  
+  
+  return (
+    <div>
+      <header>
+        <h1>Anecdote of the day</h1>
+      </header>  
+      <p>{anec}</p>
+      <p>has {currentVote} votes</p>
+      <button onClick={handleVoteclick}>vote</button>
+      <button onClick={handleAnecdoteClick}>next anecdote</button>
+      <header>
+        <h1>Anecdote with most votes</h1>
+        <p>{mostVotesAnec}</p>
+        <p>has {mostVotesAmount} votes</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
